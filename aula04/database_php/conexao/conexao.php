58899d8eb7423e3793e9036107a3a7fe9f6970d9
Redbruno7 <brunorodgers7@gmail.com>
2025-09-15 16:06:26 -0300

    aula04

---
+++ aula04/database_php/conexao/conexao.php
@@ -0,0 +1,15 @@
+<?php
+// Configurações de conexão
+$host = "localhost";        // Servidor do banco
+$user = "root";             // Usuário (padrão do XAMPP)
+$pass = "";                 // Seha (vazia no XAMPP por padrão)
+$db = "bruno_tarefas_bd";   // Nome do banco
+
+// Cria a conexão
+$conn = new mysqli($host, $user, $pass, $db);
+
+// Verifica se ocorreu algum erro
+if ($conn->connect_error) {
+    die("Erro na conexão: " . $conn->connect_error);
+}
+?>
